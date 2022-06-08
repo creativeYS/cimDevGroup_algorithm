a8ec081c5f062ba91d5390769cced07196b2f188
jch0622 <jch0622@midasit.com>
2022-06-08 12:19:33 +0900

    jch0622 / 겉넓이구하기 class header 파일 분리, Pizza Alvoloc 문제 추가

--- jch0622/16931_겉넓이구하기/StackCube.h
+++ jch0622/16931_겉넓이구하기/StackCube.h
@@ -1,11 +1,11 @@
-#include <iostream>
+#pragma once
 #include <vector>
 #include <algorithm>
 
 class StackCube
 {
     typedef std::vector<std::vector<int> > cubeInfo;
-
+    
 private:
     int depth;
     int width;
@@ -77,71 +77,3 @@ int StackCube::allSurfaceArea() const
     }
     return area;
 }
-
-
-class MapMaker
-{
-    typedef std::vector<std::vector<int> > cubeInfo;
-
-private:
-    int depth;
-    int width;
-
-    MapMaker();
-
-public:
-    MapMaker(const int & depth, const int & width);
-    MapMaker(const MapMaker & src);
-    ~MapMaker();
-
-    int getDepth() const;
-    int getWidth() const;
-
-    cubeInfo inputCubeInfo() const;
-
-    MapMaker & operator=(const MapMaker & rhs);
-
-};
-
-MapMaker::MapMaker() : depth(0), width(0) {};
-MapMaker::MapMaker(const int & depth, const int & width) : depth(depth), width(width) {};
-MapMaker::MapMaker(const MapMaker & src) : depth(src.getDepth()), width(src.getWidth()) {};
-MapMaker::~MapMaker() {};
-
-int MapMaker::getDepth() const { return this->depth; };
-int MapMaker::getWidth() const { return this->width; };
-
-MapMaker & MapMaker::operator=(const MapMaker & rhs)
-{
-    this->depth = rhs.getDepth();
-    this->width = rhs.getWidth();
-    return *this;
-}
-
-// function for algorithm
-std::vector<std::vector<int> > MapMaker::inputCubeInfo() const
-{
-    cubeInfo map(depth + 2, std::vector<int>(width + 2, 0));
-    for (int idxDepth = 1; idxDepth <= this->getDepth(); idxDepth++)
-    {
-        for (int idxWidth = 1; idxWidth <= this->getWidth(); idxWidth++)
-        {
-            std::cin >> map[idxDepth][idxWidth];
-        }
-    }
-    return map;
-}
-
-
-int main()
-{
-    int depth, width;
-    std::cin >> depth >> width;
-
-    MapMaker map(depth, width);
-    StackCube stackcube(map.getDepth(), map.getWidth(), map.inputCubeInfo());
-
-    std::cout << stackcube.allSurfaceArea();
-}
-
-// https://www.onlinegdb.com/online_c++_compiler
