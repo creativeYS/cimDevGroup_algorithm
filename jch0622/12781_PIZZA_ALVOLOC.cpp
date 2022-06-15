e279ec172e10b9cc4974c54e123e6ef7244bdbb8
jch0622 <jch0622@midasit.com>
2022-06-15 21:59:31 +0900

    jch0622 / 쓰레기슈트 정답 코드 제출 , 피자 알볼록 코드 피드백 반영본

--- jch0622/12781_PIZZA_ALVOLOC.cpp
+++ jch0622/12781_PIZZA_ALVOLOC.cpp
@@ -3,27 +3,26 @@
 class Point
 {
 private:
-    const double x;
-    const double y;
-    Point();
+	const double x;
+	const double y;
+	Point();
 
 public:
-    Point(const double & x, const double & y);
-    Point(const Point & src);
-    ~Point();
+	Point(const double & x, const double & y);
+	Point(const Point & src);
+	~Point();
 
-    double getX() const;
-    double getY() const;
+	double getX() const;
+	double getY() const;
 
-    static double outerProduct(const Point & p1, const Point & p2);
-    static bool isAcross(const Point & pa1, const Point & pa2, const Point & pb1, const Point & pb2);
+	double outerProduct(const Point & point);
 
-    Point operator-(const Point & rhs) const;
-
+	Point operator-(const Point & rhs) const;
 };
+bool isAcross(const Point & pa1, const Point & pa2, const Point & pb1, const Point & pb2);
 
 Point::Point(const double & x, const double & y) : x(x), y(y) {};
-Point::Point(const Point & src) : x(src.getX()), y(src.getY()) {}; 
+Point::Point(const Point & src) : x(src.getX()), y(src.getY()) {};
 Point::~Point() {};
 
 double Point::getX() const { return this->x; };
@@ -32,32 +31,33 @@ double Point::getY() const { return this->y; };
 // function for algorithm
 Point Point::operator-(const Point & rhs) const
 {
-    return Point(this->getX() - rhs.getX(), this->getY() - rhs.getY());
+	return Point(this->getX() - rhs.getX(), this->getY() - rhs.getY());
 }
 
-double Point::outerProduct(const Point & p1, const Point & p2)
+double Point::outerProduct(const Point & point)
 {
-    return p1.getX() * p2.getY() - p2.getX() * p1.getY();
+	return this->getX() * point.getY() - point.getX() * this->getY();
 }
 
-bool Point::isAcross(const Point & pa1, const Point & pa2, const Point & pb1, const Point & pb2)
+bool isAcross(const Point & pa1, const Point & pa2, const Point & pb1, const Point & pb2)
 {
-    double op1 = Point::outerProduct(pa2 - pa1, pb1 - pa1);
-    double op2 = Point::outerProduct(pa2 - pa1, pb2 - pa1);
-    if ( (op1 > 0 && op2 < 0) || (op1 < 0 && op2 > 0) )
-        return true;
-    return false;
-}
+	Point pa = pa2 - pa1;
+	double op1 = pa.outerProduct(pb1 - pa1);
+	double op2 = pa.outerProduct(pb2 - pa1);
 
+	if ((op1 > 0 && op2 < 0) || (op1 < 0 && op2 > 0))
+		return true;
+	return false;
+}
 
 int main()
 {
-    double x[4], y[4];
+	double x[4], y[4];
 
-    for (int i = 0; i < 4; i++)
-        std::cin >> x[i] >> y[i];
-        
-    Point point[4] = { {x[0], y[0]}, {x[1], y[1]}, {x[2], y[2]}, {x[3], y[3]} };
+	for (int i = 0; i < 4; i++)
+		std::cin >> x[i] >> y[i];
 
-    std::cout << Point::isAcross(point[0], point[1], point[2], point[3]);
+	Point point[4] = { {x[0], y[0]}, {x[1], y[1]}, {x[2], y[2]}, {x[3], y[3]} };
+
+	std::cout << isAcross(point[0], point[1], point[2], point[3]);
 }
--- jch0622/4225_쓰레기슈트/4225.cpp
+++ jch0622/4225_쓰레기슈트/4225.cpp
@@ -5,121 +5,123 @@
 #include <iomanip>
 
 #define PI acos(-1)
-#define EPSILON 0.0001
+#define EPSILON 0.00001
 
-enum Ccw { CW, CCW, PARALLEL};
+enum Ccw { CW, CCW, EQUAL, OPPOSITE };
 
 // CAD에서 실제로 polygon을 수정한다고 가정
 
 class Point2
 {
 private:
-    double x;
-    double y;
-    Point2() { }
+	double x;
+	double y;
+	Point2() { }
 
 public:
-    Point2(const double & x, const double & y)  : x(x), y(y) { }
-    Point2(const Point2 & src) : x(src.getX()), y(src.getY()) { }
-    ~Point2() { }
+	Point2(const double & x, const double & y) : x(x), y(y) { }
+	Point2(const Point2 & src) : x(src.getX()), y(src.getY()) { }
+	~Point2() { }
 
-    double getX() const { return this->x; }
-    double getY() const { return this->y; }
-    void setX(const int & x) { this->x = x; }
-    void setY(const int & y) { this->y = y; }
+	double getX() const { return this->x; }
+	double getY() const { return this->y; }
+	void setX(const int & x) { this->x = x; }
+	void setY(const int & y) { this->y = y; }
 
-    Point2 & operator=(Point2 const & rhs)
-    {
-        this->x = rhs.getX();
-        this->y = rhs.getY();
-        return *this;
-    }
+	Point2 & operator=(Point2 const & rhs)
+	{
+		this->x = rhs.getX();
+		this->y = rhs.getY();
+		return *this;
+	}
 
-    Point2 operator-(const Point2 & rhs) const
-    {
-        return Point2(this->getX() - rhs.getX(), this->getY() - rhs.getY());
-    }
+	Point2 operator-(const Point2 & rhs) const
+	{
+		return Point2(this->getX() - rhs.getX(), this->getY() - rhs.getY());
+	}
 
-    // 0으로 나눌 경우에 대한 예외처리 필요해보임
-    Point2 operator/(const double & value) const
-    {
-        return Point2(this->getX() / value, this->getY() / value);
-    }
+	// 0으로 나눌 경우에 대한 예외처리 필요해보임
+	Point2 operator/(const double & value) const
+	{
+		return Point2(this->getX() / value, this->getY() / value);
+	}
 
-    // 거리
-    double dist(const Point2 & point) const
-    {
-        double x = this->x - point.x;
-        double y = this->y - point.y;
-        return sqrt(x * x + y * y);
-    }
+	// 거리
+	double dist(const Point2 & point) const
+	{
+		double x = this->x - point.x;
+		double y = this->y - point.y;
+		return sqrt(x * x + y * y);
+	}
 
-    double dist() const
-    {
-        return sqrt(this->x * this->x + this->y * this->y);
-    }
+	double dist() const
+	{
+		return sqrt(this->x * this->x + this->y * this->y);
+	}
 
-    // 방향벡터화
-    Point2 normalize() const
-    {
-        return (*this) / dist();
-    }
+	// 방향벡터화
+	Point2 normalize() const
+	{
+		return (*this) / dist();
+	}
 
-    // 현재 점을 기준으로 p1과 p2사이의 각도 찾기
-    double findAngle(const Point2 & p1, const Point2 & p2) const
-    {
-        const Point2 normalVector1 = (p1 - *this).normalize();
-        const Point2 normalVector2 = (p2 - *this).normalize();
+	// 현재 점을 기준으로 p1과 p2사이의 각도 찾기
+	double findAngle(const Point2 & p1, const Point2 & p2) const
+	{
+		const Point2 normalVector1 = (p1 - *this).normalize();
+		const Point2 normalVector2 = (p2 - *this).normalize();
 
-        double cosTheta = normalVector1.innerProduct(normalVector2);
-        return acos(cosTheta) * 180 / PI;
-    }
+		double cosTheta = normalVector1.innerProduct(normalVector2);
+		return acos(cosTheta);
+	}
 
-    // 현재 점을 기준으로 p1에서 p2의 방향 찾기
-    int ccw(const Point2 & p1, const Point2 & p2) const
-    {
-        double vec1 = this->outerProductZValue(p1, p2);
-        if (vec1 > 0)
-            return CCW;
-        else if (vec1 < 0)
-            return CW;
-        else
-            return PARALLEL;
-    }
+	// 현재 점을 기준으로 p1에서 p2의 방향 찾기
+	int ccw(const Point2 & p1, const Point2 & p2) const
+	{
+		double vec1 = this->outerProductZValue(p1, p2);
+		if (vec1 > 0)
+			return CCW;
+		else if (vec1 < 0)
+			return CW;
+		else if (this->findAngle(p1, p2) < EPSILON)
+			return EQUAL;
+		else
+			return OPPOSITE;
+	}
 
-    // 내적값
-    double innerProduct(const Point2 & p1, const Point2 & p2) const
-    {
-        const Point2 vec1(p1 - (*this));
-        const Point2 vec2(p2 - (*this));
-        return vec1.getX() * vec2.getX() + vec1.getY() * vec2.getY();
-    }
+	// 내적값
+	double innerProduct(const Point2 & p1, const Point2 & p2) const
+	{
+		const Point2 vec1(p1 - (*this));
+		const Point2 vec2(p2 - (*this));
+		return vec1.getX() * vec2.getX() + vec1.getY() * vec2.getY();
+	}
 
-    double innerProduct(const Point2 & vec) const
-    {
-        return this->getX() * vec.getX() + this->getY() * vec.getY();
-    }
+	double innerProduct(const Point2 & vec) const
+	{
+		return this->getX() * vec.getX() + this->getY() * vec.getY();
+	}
 
-    // 외적 z값
-    double outerProductZValue(const Point2 & p1, const Point2 & p2) const
-    {
-        const Point2 vec1(p1 - (*this));
-        const Point2 vec2(p2 - (*this));
-        return vec1.getX() * vec2.getY() - vec2.getX() * vec1.getY();
-    }
+	// 외적 z값
+	double outerProductZValue(const Point2 & p1, const Point2 & p2) const
+	{
+		const Point2 vec1(p1 - (*this));
+		const Point2 vec2(p2 - (*this));
+		return vec1.getX() * vec2.getY() - vec2.getX() * vec1.getY();
+	}
 
-    double outerProductZValue(const Point2 & vec) const
-    {
-        return this->getX() * vec.getY() - vec.getX() * this->getY();
-    }
+	double outerProductZValue(const Point2 & vec) const
+	{
+		return this->getX() * vec.getY() - vec.getX() * this->getY();
+	}
 
-    // 점과 직선 사이의 거리
-    double lineToPointDistance(const Point2 & p1, const Point2 & p2) const
-    {
-        // this-> 해당 점
-        double angle = p1.findAngle(*this, p2);
-        return p1.dist(*this) * sin(angle * PI / 180);
-    }
+	// 점과 직선 사이의 거리
+	double lineToPointDistance(const Point2 & p1, const Point2 & p2) const
+	{
+		// this-> 해당 점
+		double angle = p1.findAngle(*this, p2);
+		return p1.dist(*this) * sin(angle);
+	}
 };
 
 
@@ -127,139 +129,149 @@ public:
 class Polygon
 {
 private:
-    const int numberOfSide;
-    std::vector<Point2> node;
-    Polygon();
+	const int numberOfSide;
+	std::vector<Point2> node;
+	Polygon();
 
 public:
 
-    Polygon(const int & numberOfSide, const std::vector<Point2> & node) : numberOfSide(numberOfSide), node(node) { }
-    Polygon(const Polygon & src) : numberOfSide(src.getNumberOfSide()), node(src.getNode()) {}
-    ~Polygon() {}
+	Polygon(const int & numberOfSide, const std::vector<Point2> & node) : numberOfSide(numberOfSide), node(node) { }
+	Polygon(const Polygon & src) : numberOfSide(src.getNumberOfSide()), node(src.getNode()) {}
+	~Polygon() {}
 
-    Point2 & operator[](const int & index)
-    {
-        return node[index];
-    }
+	Point2 & operator[](const int & index)
+	{
+		return node[index];
+	}
 
-    const int getNumberOfSide() const { return numberOfSide; }
-    const std::vector<Point2> getNode() const { return node; }
+	const int getNumberOfSide() const { return numberOfSide; }
+	const std::vector<Point2> getNode() const { return node; }
 
-    // 원하는 답
-    double polygonMinWidth() const
-    {
-        // 볼록껍질의 지점까지 이동하는 함수
-        std::pair<int, int> index = this->findFirstConvexPoint();
-        int startIndex = index.first;
-        int nextIndex = index.second;
+	// 원하는 답
+	double polygonMinWidth() const
+	{
+		// 볼록껍질의 지점까지 이동하는 함수
+		std::pair<int, int> index = this->findFirstConvexPoint();
+		int startIndex = index.first;
+		int nextIndex = index.second;
+		// std::cout << "<" << startIndex << " " << nextIndex << ":" << polygonWidth(startIndex, nextIndex) << ">\n";
 
-        // 첫 시작만 반복문 밖에서
-        double minWidth = this->polygonWidth(startIndex, nextIndex);
-        int curIndex = nextIndex;
+		// 첫 시작만 반복문 밖에서
+		double minWidth = this->polygonWidth(startIndex, nextIndex);
+		int curIndex = nextIndex;
 
-        while (startIndex != curIndex)
-        {
-            nextIndex = this->findConvexPoint(curIndex);
-            minWidth = std::min(polygonWidth(curIndex, nextIndex), minWidth);
-            curIndex = nextIndex;
-        }
-        return minWidth;
-    }
+		while (startIndex != curIndex)
+		{
+			nextIndex = this->findConvexPoint(curIndex);
+			minWidth = std::min(polygonWidth(curIndex, nextIndex), minWidth);
+			// std::cout << "<" << startIndex << " " << curIndex << " " << nextIndex << ":" << polygonWidth(curIndex, nextIndex) << ">\n";
+			curIndex = nextIndex;
+		}
+		return minWidth;
+	}
 
-    int findNextIndex(const int & index) const
-    {
-        return this->findConvexPoint(index);
-    }
+	int findNextIndex(const int & index) const
+	{
+		return this->findConvexPoint(index);
+	}
 
-    // 특정 점 -> 선분, 선분을 기준으로 가장 짧은 폭
-    double polygonWidth(const int & index, const int & nextIndex) const
-    {
-        double maxWidth = 0.0;
-        for (int i = 0; i < this->numberOfSide; i++)
-        {
-            if (i == index || i == nextIndex)
-                continue;
-            double dist = this->node[i].lineToPointDistance(this->node[index], this->node[nextIndex]);
-            maxWidth = std::max(maxWidth, dist);
-        }
-        return maxWidth;
-    }
+	// 특정 점 -> 선분, 선분을 기준으로 가장 짧은 폭
+	double polygonWidth(const int & index, const int & nextIndex) const
+	{
+		double maxWidth = 0.0;
+		for (int i = 0; i < this->numberOfSide; i++)
+		{
+			if (i == index || i == nextIndex)
+				continue;
+			double dist = this->node[i].lineToPointDistance(this->node[index], this->node[nextIndex]);
+			maxWidth = std::max(maxWidth, dist);
+		}
+		return maxWidth;
+	}
 
-    // find 볼록껍질까지의 지점
-    std::pair<int, int> findFirstConvexPoint() const
-    {
-        for (int i = 0; i < this->numberOfSide; i++)
-        {
-            int nextIndex = this->findConvexPoint(i);
-            if (nextIndex >= 0)
-                return std::make_pair(i, nextIndex);
-        }
-        return std::make_pair(-1, -1);
-    }
+	// find 볼록껍질까지의 지점
+	std::pair<int, int> findFirstConvexPoint() const
+	{
+		for (int i = 0; i < this->numberOfSide; i++)
+		{
+			int nextIndex = this->findConvexPoint(i);
+			if (nextIndex >= 0)
+				return std::make_pair(i, nextIndex);
+		}
+		return std::make_pair(-1, -1);
+	}
 
-    // 볼록 껍질의 Node인지 판별
-    int findConvexPoint(const int & index) const
-    {
+	// 볼록 껍질의 Node인지 판별
+	int findConvexPoint(const int & index) const
+	{
+		const int indexNext = (index + 1) % this->numberOfSide;
+		std::vector<double> angleVec = { 0.0, 0.0, 0.0, 0.0 };
+		std::vector<int> idxVec = { -1, -1, indexNext, -1 };
+		double parallelDist = 0;
 
-        const int indexNext = (index + 1) % this->numberOfSide;
-        std::vector<double> angleVec = {0.0, 0.0, 0.0};
-        std::vector<int> idxVec = {-1, indexNext, -1};
+		for (int i = 0; i < this->numberOfSide; i++)
+		{
+			if (i == index || i == indexNext)
+				continue;
+			const double angle = this->node[index].findAngle(this->node[indexNext], this->node[i]);
+			const int ccwValue = this->node[index].ccw(this->node[indexNext], this->node[i]);
 
-        for (int i = 0; i < this->numberOfSide; i++)
-        {
-            if (i == index || i == indexNext)
-                continue;
-            const double angle = this->node[index].findAngle(this->node[indexNext], this->node[i]);
-            const int ccwValue = this->node[index].ccw(this->node[indexNext], this->node[i]);
+			if (ccwValue == EQUAL && parallelDist < this->node[index].dist(this->node[i]))
+			{
+				parallelDist = this->node[index].dist(this->node[i]);
+				idxVec[ccwValue] = i;
+			}
+			else if (angleVec[ccwValue] < angle)
+			{
+				angleVec[ccwValue] = angle;
+				idxVec[ccwValue] = i;
+			}
+		}
+		// std::cout << "[" << angleVec[CW] << " " << angleVec[CCW] << " " << angleVec[EQUAL] << " " << angleVec[OPPOSITE] << "]\n";
+		// std::cout << "[" << idxVec[CW] << " " << idxVec[CCW] << " " << idxVec[EQUAL] << " " << idxVec[OPPOSITE] << "]\n";
 
-            if (angleVec[ccwValue] < angle)
-            {
-                angleVec[ccwValue] = angle;
-                idxVec[ccwValue] = i;
-            }
-        }
-        if (angleVec[PARALLEL] + EPSILON >= 180 || angleVec[CW] + angleVec[CCW] > 180)
-            return -1;
-        else
-        {
-            if (idxVec[CW] >= 0)
-                return idxVec[CW];
-            else
-                return indexNext;
-        }
-    }
+		if (idxVec[OPPOSITE] >= 0 || angleVec[CW] + angleVec[CCW] > PI + EPSILON)
+			return -1;
+		else if (idxVec[CW] >= 0)
+				return idxVec[CW];
+		else
+			return idxVec[EQUAL];
+	}
 };
 
 int main()
 {
-    std::vector<double> answer;
+	std::vector<double> answer;
 
-    int numberOfSide;
-    std::cin >> numberOfSide;
+	int numberOfSide;
+	std::cin >> numberOfSide;
 
-    while (numberOfSide > 0)
-    {
-        std::vector<Point2> node(numberOfSide, Point2(0, 0));
-        for (int i = 0; i < numberOfSide; i++)
-        {
-            int x, y;
-            std::cin >> x >> y;
-            node[i] = Point2(x, y);
-        }
-        Polygon polygon(numberOfSide, node);
-        answer.push_back(polygon.polygonMinWidth());
-        std::cin >> numberOfSide;
-    }
+	while (numberOfSide > 0)
+	{
+		std::vector<Point2> node(numberOfSide, Point2(0, 0));
+		for (int i = 0; i < numberOfSide; i++)
+		{
+			int x, y;
+			std::cin >> x >> y;
+			node[i] = Point2(x, y);
+		}
+		Polygon polygon(numberOfSide, node);
+		answer.push_back(polygon.polygonMinWidth());
+		std::cin >> numberOfSide;
+	}
 
-    for (int i = 0; i < answer.size(); i++)
-    {
-        std::cout.precision(2);
-		std::cout.setf( std::ios::fixed );
-        if (answer[i]*100 - static_cast<int>(answer[i]*100) > 0.1)
-            std::cout << "Case " << i+1 << ": " << ceil(answer[i]*100)/100 << std::endl;
-        else
-            std::cout << "Case " << i+1 << ": " << answer[i] << std::endl;
-    }
+	for (int i = 0; i < answer.size(); i++)
+	{
+		// std::cout.precision(20);
+		// std::cout << "Case " << i+1 << ": " << answer[i] << std::endl;
+		std::cout.precision(2);
+		std::cout.setf(std::ios::fixed);
+		std::cout << "Case " << i + 1 << ": " << ceil((answer[i] - EPSILON) * 100) / 100 << std::endl;
+		// if (answer[i]*100 - static_cast<int>(answer[i]*100) > 0.1)
+		//     std::cout << "Case " << i+1 << ": " << ceil(answer[i]*100)/100 << std::endl;
+		// else
+		//     std::cout << "Case " << i+1 << ": " << answer[i] << std::endl;
+	}
 }
 
 // 벡터 각도 구하기
@@ -268,48 +280,15 @@ int main()
 // 두 벡터의 각도가 180 초과 -> 오목다각형 부분이라 판별하지 않음
 // noexcept 이용해서 복사생성자 안만드는거도 참고해볼 필요가 있을듯
 
-/* 
-3
-0 0
-3 0
-0 4
-3
-0 0
-0 4
-3 0
+/*
 4
-0 10
-10 0
-20 10
-10 20
-4
-10 20
-20 10
-10 0
-0 10
-5
 0 0
--1 10
-11 12
-5 5
-10 -1
-5
-5 5
-10 -1
-0 0
--1 10
-11 12
+1 0
+10000 1
+9999 1
 0
 
-9
-1 2
-2 6
-3 9
-5 10
-11 10
-9 3
-7 6
-5 -1
-0 0
-0
+output : 0.01
+myoutput : 0.00
+
 */
